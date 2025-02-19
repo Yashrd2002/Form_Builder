e719892e48c315647323a24e2af713f4b74d9b3d
Yash2002 <yashrajiv.deshmukh2021@vitstudent.ac.in>
2025-02-19 10:09:47 +0530

    final1

--- app/form/[id]/preview/page.tsx
+++ app/form/[id]/preview/page.tsx
@@ -18,6 +18,7 @@ import {
 } from "@mui/material";
 import { useParams } from "next/navigation";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import Sidebar from "@/components/Sidebar";
 
 export default function FormPreviewPage() {
   const { id } = useParams();
@@ -65,61 +66,64 @@ export default function FormPreviewPage() {
 
   return (
     <ProtectedRoute>
-      <Container maxWidth="md">
-        <Typography variant="h4" gutterBottom sx={{ mt: 4 }}>
-          Preview: {form.title}
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {form.description}
-        </Typography>
-        <Box sx={{ mt: 3 }}>
-          {form.questions.map((question: any, index: number) => (
-            <Card key={question.id} sx={{ mb: 3 }}>
-              <CardContent>
-                <Typography variant="h6" gutterBottom>
-                  {index + 1}. {question.questionText}
-                </Typography>
-                {question.type === "text" && (
-                  <TextField fullWidth variant="outlined" disabled />
-                )}
-                {question.type === "multipleChoice" && (
-                  <Box>
-                    {question.options?.map((option: string, i: number) => (
-                      <FormControlLabel
-                        key={i}
-                        control={<Checkbox disabled />}
-                        label={option}
-                      />
-                    ))}
-                  </Box>
-                )}
-                {question.type === "checkbox" && (
-                  <Box>
-                    {question.options?.map((option: string, i: number) => (
-                      <FormControlLabel
-                        key={i}
-                        control={<Checkbox disabled />}
-                        label={option}
-                      />
-                    ))}
-                  </Box>
-                )}
-                {question.type === "fileUpload" && (
-                  <TextField
-                    fullWidth
-                    type="file"
-                    disabled
-                    inputProps={{ accept: question.fileTypes?.join(",") }}
-                  />
-                )}
-                {question.type === "shortAnswer" && (
-                  <TextField fullWidth variant="outlined" disabled />
-                )}
-              </CardContent>
-            </Card>
-          ))}
-        </Box>
-      </Container>
+      <div className="flex h-screen">
+        <Sidebar />
+        <Container maxWidth="lg" className="flex-grow p-8 overflow-y-auto">
+          <Typography variant="h4" gutterBottom sx={{ mt: 4 }}>
+            Preview: {form.title}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            {form.description}
+          </Typography>
+          <Box sx={{ mt: 3 }}>
+            {form.questions.map((question: any, index: number) => (
+              <Card key={question.id} sx={{ mb: 3 }}>
+                <CardContent>
+                  <Typography variant="h6" gutterBottom>
+                    {index + 1}. {question.questionText}
+                  </Typography>
+                  {question.type === "text" && (
+                    <TextField fullWidth variant="outlined" disabled />
+                  )}
+                  {question.type === "multipleChoice" && (
+                    <Box>
+                      {question.options?.map((option: string, i: number) => (
+                        <FormControlLabel
+                          key={i}
+                          control={<Checkbox disabled />}
+                          label={option}
+                        />
+                      ))}
+                    </Box>
+                  )}
+                  {question.type === "checkbox" && (
+                    <Box>
+                      {question.options?.map((option: string, i: number) => (
+                        <FormControlLabel
+                          key={i}
+                          control={<Checkbox disabled />}
+                          label={option}
+                        />
+                      ))}
+                    </Box>
+                  )}
+                  {question.type === "fileUpload" && (
+                    <TextField
+                      fullWidth
+                      type="file"
+                      disabled
+                      inputProps={{ accept: question.fileTypes?.join(",") }}
+                    />
+                  )}
+                  {question.type === "shortAnswer" && (
+                    <TextField fullWidth variant="outlined" disabled />
+                  )}
+                </CardContent>
+              </Card>
+            ))}
+          </Box>
+        </Container>
+      </div>
     </ProtectedRoute>
   );
 }
